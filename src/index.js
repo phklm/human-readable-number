b82c36db6235562d9bbaa87d9f2a2196d0afc640
phkl <phkl@tuta.io>
2020-03-22 21:17:48 +0300

    add function

--- src/index.js
+++ src/index.js
@@ -40,12 +40,12 @@ module.exports = function toReadable (number) {
 if (number < 10) return firstNum[number];
 if (number == 10) return thirdNum[1];
 if (number > 10 && number < 21) return secondNum[number.toString()[1]];
-if (number > 20 && number < 100 && number.toString()[1] == "0") return thirdNum[number.toString()[0]];
-if (number > 20 && number < 100 && number.toString()[1] != "0") return thirdNum[number.toString()[0]] + " " + firstNum[number.toString()[1]];
-if (number > 99 && number < 1000 && number.toString()[1] == "0" && number.toString()[2] == "0") return firstNum[number.toString()[0]] + " hundred";
-if (number > 99 && number < 1000 && number.toString()[1] == "0") return firstNum[number.toString()[0]] + " hundred " + firstNum[number.toString()[2]];
-if (number > 99 && number < 1000 && number.toString()[1] == "1" && number.toString()[2] == "0") return firstNum[number.toString()[0]] + " hundred " + thirdNum[1];
-if (number > 99 && number < 1000 && number.toString()[1] == "1") return firstNum[number.toString()[0]] + " hundred " + secondNum[number.toString()[2]];
-if (number > 99 && number < 1000 && number.toString()[2] == "0") return firstNum[number.toString()[0]] + " hundred " + thirdNum[number.toString()[1]];
-else return firstNum[number.toString()[0]] + " hundred " + thirdNum[number.toString()[1]] + " " + firstNum[number.toString()[2]];
+if (number > 20 && number < 100 && number.toString()[1] == '0') return thirdNum[number.toString()[0]];
+if (number > 20 && number < 100 && number.toString()[1] != '0') return thirdNum[number.toString()[0]] + ' ' + firstNum[number.toString()[1]];
+if (number > 99 && number < 1000 && number.toString()[1] == '0' && number.toString()[2] == '0') return firstNum[number.toString()[0]] + ' hundred';
+if (number > 99 && number < 1000 && number.toString()[1] == '0') return firstNum[number.toString()[0]] + ' hundred ' + firstNum[number.toString()[2]];
+if (number > 99 && number < 1000 && number.toString()[1] == '1' && number.toString()[2] == '0') return firstNum[number.toString()[0]] + ' hundred ' + thirdNum[1];
+if (number > 99 && number < 1000 && number.toString()[1] == '1') return firstNum[number.toString()[0]] + ' hundred ' + secondNum[number.toString()[2]];
+if (number > 99 && number < 1000 && number.toString()[2] == '0') return firstNum[number.toString()[0]] + ' hundred ' + thirdNum[number.toString()[1]];
+else return firstNum[number.toString()[0]] + ' hundred ' + thirdNum[number.toString()[1]] + ' ' + firstNum[number.toString()[2]];
 }
